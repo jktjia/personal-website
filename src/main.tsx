a7a362c203c23eead5950fdec62ef613139b6b08
jktjia <jamiekai.san@gmail.com>
2025-01-31 10:35:13 -0500

    feat: added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "@/pages/app";
+import CatProvider from "./components/cat-provider.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import About from "./pages/about.tsx";
+import Cats from "./pages/cats.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <CatProvider count={3}>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cats" element={<Cats />} />
+        </Routes>
+      </CatProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
